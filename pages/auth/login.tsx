bb9d09d5f60cec7e02a8835ca67284792d1455de
Dániel B. Papp <dpapp@odu.edu>
2022-08-13 21:20:00 -0400

    feat(general): :lipstick: Matched home and login pages to the mockup

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,21 +1,20 @@
 import React, { useState } from 'react'
+import { useRouter } from 'next/router'
 import Auth from '../../components/Auth'
 import { useAuth } from '../../context/auth'
-import { useRouter } from 'next/router'
 import Loader from '../../components/Loader'
+import { useUser } from '@supabase/auth-helpers-react'
 
 function Login() {
 	const [loading, setLoading] = useState(true)
-	const { user } = useAuth()
+	const { user } = useUser()
 	const router = useRouter()
 
 	if (user) {
 		router.push('/')
 	}
 
-	return loading ? (
-		<Loader />
-	) : (
+	return (
 		<>
 			<Auth />
 		</>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,62 @@
-import { useState, useEffect } from 'react'
-import { supabase } from '../utils/supabaseClient'
-import Auth from '../components/Auth'
-// import Account from '../components/Account'
+import { useState } from 'react'
 import Link from 'next/link'
-import { useAuth } from '../context/auth'
+import { useUser } from '@supabase/auth-helpers-react'
+import { useRouter } from 'next/router'
+import Image from 'next/image'
+import { FaGamepad } from 'react-icons/fa'
+import { BsFillPentagonFill } from 'react-icons/bs'
+import { AiOutlineArrowRight } from 'react-icons/ai'
 
 export default function Home() {
-	const [session, setSession] = useState(null)
-	const { user } = useAuth()
+	const { user } = useUser()
+	const router = useRouter()
 
 	if (user) {
-		return (
-			<>
-				<h1>Welcome {user.username}</h1>
-				<Link href={`user/${user.id}`}>
-					<a>Account</a>
-				</Link>
-			</>
-		)
-	} else {
-		return (
-			<>
-				<h1>Log in to access the portal</h1>
-				<Link href={`auth/login`}>
-					<a className="bg-og-500">Login</a>
-				</Link>
-			</>
-		)
+		router.push(`/user/${user.id}`)
 	}
+	return (
+		<section className="flex h-full overflow-hidden">
+			<div className="w-1/2 flex flex-col md:gap-16 items-center justify-center">
+				<h1 className="text-7xl font-extrabold font-lato drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] shadow-black">
+					Are you an og?
+				</h1>
+				<div className="border-og-purple-500 border rounded bg-gray-50 px-7 py-5 flex items-center relative gap-5 shadow">
+					<div className="flex items-center justify-center my-5 mx-8">
+						<FaGamepad size={35} className="z-10 text-white" />
+						<BsFillPentagonFill
+							size={85}
+							className="text-og-500 absolute z-0"
+						/>
+					</div>
+					<div className="font-inter">
+						<h2 className="text-lg font-medium">
+							Make an Account and Get Started
+						</h2>
+						<p className="text-sm text-gray-400 font-normal">
+							Select games, record stats, join teams & get
+							recruited!
+						</p>
+					</div>
+					<Link href="/auth/login">
+						<a>
+							<AiOutlineArrowRight
+								size={30}
+								className="text-og-500"
+							/>
+						</a>
+					</Link>
+				</div>
+			</div>
+			<div className="w-1/2 relative">
+				<div className="relative h-full z-10">
+					<span
+						style={{
+							backgroundImage: `url('/lines.png')`,
+						}}
+						className="absolute -top-20 w-full h-full"
+					></span>
+				</div>
+			</div>
+		</section>
+	)
 }
